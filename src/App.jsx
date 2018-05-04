c154a499ae64b53316bbace944334788f16c1e56
ral51 <oraldalay@gmail.com>
2018-05-04 12:22:41 -0400

    run prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
 import React, { Component } from 'react';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.state = {
+      celcius: 0,
+      fahrenheit: 0
+    };
+  }
 
-	constructor(props) {
-		super(props);
-		this.handleInputChange = this.handleInputChange.bind(this);
-		this.state = {
-			celcius: 0,
-			fahrenheit: 0
-		};
-	}
+  handleInputChange(evt) {
+    var c = evt.currentTarget.value;
+    this.setState({
+      celcius: c,
+      fahrenheit: this.toFahrenheit(c)
+    });
+  }
 
-	handleInputChange(evt) {
-		var c = evt.currentTarget.value;
-		this.setState({
-			celcius: c,
-			fahrenheit: this.toFahrenheit(c)
+  toFahrenheit(celsius) {
+    return celsius * 9 / 5 + 32;
+  }
 
-		})
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter temperature in Celcius"
+          onChange={this.handleInputChange}
+        />
 
-	}
-
-	toFahrenheit(celsius) {
-		return (celsius * 9 / 5) + 32;
-	}
-
-	render() {
-		return (
-			<div>
-				<input type='text' placeholder='Enter temperature in Celcius' onChange={this.handleInputChange} />
-
-				<div>
-					{`${this.state.fahrenheit} F`}
-				</div>
-			</div>
-		)
-
-	}
-
+        <div>{`${this.state.fahrenheit} F`}</div>
+      </div>
+    );
+  }
 }
